39a85eec92fcb4b21902befbcadf8647f5d3c960
EmersonJPJ <106222766+EmersonJPJ@users.noreply.github.com>
2022-12-03 23:53:13 -0600

    Add files via upload

---
+++ Hilos/src/Hilo1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author EMERSON
+ */
+public class Hilo1 implements Runnable{
+    
+    public void run() {
+        try{
+        Thread.sleep(500);
+        }
+    }
+    
+}
